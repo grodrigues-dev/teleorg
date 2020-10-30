8073ad2f6d764187ac0934cfaca434f57f4adccc
Gabriel Rodrigues <gabriel.rodrigues@opah.com.br>
2020-10-30 00:54:10 -0300

    iniciando estilos da home

--- pages/home.js
+++ pages/home.js
@@ -1,13 +1,16 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import Header from '../shared/components/Header';
 
 import {
+    Image,
     StyleSheet,
-    View,
     Text,
+    TextInput,
+    TouchableHighlight,
+    View,
 } from 'react-native';
 
 import { getOrgaos } from '../shared/services/api';
-
 
 export default class Home extends Component {
 
@@ -32,7 +35,42 @@ export default class Home extends Component {
     render() {
         return (
             <View style={styles.container}>
-                <Text>teleorg</Text>
+                <Header />
+                <View style={styles.pesquisaContainer}>
+                    <View style={styles.pesquisa}>
+                        <Text style={styles.textoPesquisa}>
+                            Digite o código de {"\n"} rastreamento
+                        </Text>
+                        <View style={styles.containerInput}>
+                            <View style={styles.backgroundInput}>
+                                <TextInput
+                                    style={styles.textInput} />
+                                <Image style={styles.icon} source={require('../assets/lupa.png')} />
+                            </View>
+                        </View>
+                        <View style={styles.linha} />
+                        <View style={styles.containerOrgaos}>
+                            <Image style={styles.iconOrgaos} source={require('../assets/rim.png')} />
+                            <Image style={styles.iconOrgaos} source={require('../assets/pulmao.png')} />
+                            <Image style={styles.iconOrgaos} source={require('../assets/coracao.png')} />
+                            <Image style={styles.iconOrgaos} source={require('../assets/figado.png')} />
+                        </View>
+                    </View>
+                    <View style={styles.containerBotoes}>
+                        <TouchableHighlight style={styles.btnFiltro}>
+                            <React.Fragment>
+                                <Text style={styles.textoBotao}>Filtro</Text>
+                                <Image style={styles.icoBotao} source={require('../assets/filtro.png')} />
+                            </React.Fragment>
+                        </TouchableHighlight>
+                        <TouchableHighlight style={styles.btnOrdenar}>
+                            <React.Fragment>
+                                <Text style={styles.textoBotao}>Ordenar</Text>
+                                <Image style={styles.icoBotao} source={require('../assets/ordenar.png')} />
+                            </React.Fragment>
+                        </TouchableHighlight>
+                    </View>
+                </View>
             </View>
         )
     }
@@ -42,42 +80,107 @@ export default class Home extends Component {
 
 const styles = StyleSheet.create({
     container: {
+        backgroundColor: '#FAF5F5'
+    },
+    pesquisaContainer: {
         alignItems: "center",
         backgroundColor: '#1538A1',
         padding: 20,
-        marginTop: 100
+        height: 150,
     },
-    text: {
-        color: '#fff',
-        fontSize: 30,
-        fontWeight: "bold"
+    pesquisa: {
+        backgroundColor: '#fff',
+        height: 290,
+        width: 380,
+        borderRadius: 5,
+        display: 'flex',
+        alignItems: 'center'
     },
-    input: {
-        backgroundColor: "#fff",
+    textoPesquisa: {
+        color: '#1538A1',
+        textAlign: 'center',
+        fontWeight: 'bold',
+        fontSize: 18,
+        marginTop: 10
+    },
+    containerInput: {
+        backgroundColor: '#C91D14',
+        height: 70,
+        width: 300,
         marginTop: 20,
-        borderRadius: 5,
-        width: 288,
-        padding: 10,
-        fontSize: 20
+        borderRadius: 30,
+        display: 'flex',
+        flexDirection: 'row',
+        padding: 10
     },
-    button: {
-        borderColor: "#fff",
-        borderStyle: "solid",
-        borderWidth: 2,
-        borderRadius: 5,
-        marginTop: 25,
-        width: 188,
-        textAlign: "center",
-        padding: 6,
+    backgroundInput: {
+        backgroundColor: '#fff',
+        width: 280,
+        display: 'flex',
+        flexDirection: 'row',
+        justifyContent: "space-between",
+        padding: 5
+    },
+    icon: {
+        width: 35,
+        height: 35
+    },
+    iconOrgaos: {
+        width: 60,
+        height: 60
+    },
+    textInput: {
+        width: 230,
+    },
+    linha: {
+        width: 300,
+        height: 2,
+        backgroundColor: '#000',
+        marginTop: 20,
+        marginBottom: 20
+    },
+    containerOrgaos: {
+        display: 'flex',
+        flexDirection: 'row',
+        width: 300,
+        justifyContent: "space-between"
+    },
+    containerBotoes: {
+        display: 'flex',
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        width: 380,
+        marginTop: 20,
+        height: 50
+    },
+    btnFiltro: {
+        backgroundColor: '#F79114',
+        display: "flex",
+        flexDirection: 'row',
+        justifyContent: 'space-around',
+        alignItems: 'center',
+        padding: 20,
+        width: 160,
+        borderRadius: 40
+    },
+    btnOrdenar: {
+        backgroundColor: '#21A115',
+        display: "flex",
+        flexDirection: 'row',
+        justifyContent: 'space-around',
+        alignItems: 'center',
+        padding: 20,
+        width: 160,
+        borderRadius: 40
+    },
+    textoBotao: {
         color: '#fff',
-        fontSize: 24,
+        fontSize: 18,
         fontWeight: 'bold'
     },
-    link: {
-        color: "#fff",
-        fontSize: 18,
-        textDecorationLine: "underline",
-        paddingTop: 15
+    icoBotao: {
+        width: 25,
+        height: 20
     }
 })
 
---
+++ shared/components/Header/index.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import {
+    Image,
+    StyleSheet,
+    View
+} from 'react-native';
+
+export default function Header() {
+    return (
+        <View style={styles.header}>
+            <Image style={styles.logo} source={require('../../../assets/logo.png')} />
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    header: {
+        display: 'flex', 
+        flexDirection: 'row',
+        backgroundColor: '#1538A1',
+        height: 20,
+        paddingTop: 5,
+        justifyContent: 'center'
+    },
+    logo: {
+        height: 20,
+        width: 120
+    }
+})
+
